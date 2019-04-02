fab0971230684fb0aebc1d9d6a2cb537f2f24aa3
Lindsey Sandberg <sandbergy@yahoo.com>
2019-04-02 11:10:06 -0500

    changes pictures

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ class Footer extends React.Component {
                     <ul className="icons">
                         <li><a href="" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
                         <li><a href="https://github.com/lindseymaae" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="lmsandberg@stkate.edu" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/lindsey-sandberg-611346123/"><span className="label">LinkedIn</span></a></li>
                     </ul>
                     <ul className="copyright">
                         <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
